d77e1142c5e349bf891eddffdf733ad65a32b3ea
Christian Hofstaedtler <christian@hofstaedtler.name>
2013-01-02 00:10:18 +0100

    tidy up base stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @stats = {
+      :customers => Customer.count
+    }
+    respond_to do |format|
+      format.html
+      format.json { render :json => @stats }
+    end
   end
 end

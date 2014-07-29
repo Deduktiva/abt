6c53ac5916d8bfa883c695e148fa4612a3d7003a
Christian Hofstaedtler <christian@hofstaedtler.name>
2014-07-30 01:16:11 +0200

    Upgrade to rails 4.0

--- app/models/sales_tax_customer_class.rb
+++ app/models/sales_tax_customer_class.rb
@@ -1,7 +1,7 @@
 class SalesTaxCustomerClass < ActiveRecord::Base
   attr_accessible :invoice_note, :name
-  has_many :sales_tax_rates, :dependent => :restrict
-  has_many :customers, :dependent => :restrict
+  has_many :sales_tax_rates, :dependent => :restrict_with_exception
+  has_many :customers, :dependent => :restrict_with_exception
 
   validates :name, :presence => true
 end

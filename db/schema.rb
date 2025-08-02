056d14bddf6d9b1bc0ba67aca7cf93a87e86ff40
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2025-08-02 20:04:25 +0200

    Update schema.rb after successful migrations

--- db/schema.rb
+++ db/schema.rb
@@ -2,27 +2,26 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2014_12_26_231925) do
-
+ActiveRecord::Schema[7.1].define(version: 2014_12_26_231925) do
   create_table "attachments", force: :cascade do |t|
-    t.string "title", limit: 255
-    t.string "filename", limit: 255
-    t.string "content_type", limit: 255
+    t.string "title"
+    t.string "filename"
+    t.string "content_type"
     t.binary "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
-    t.string "matchcode", limit: 255
+    t.string "matchcode"
     t.text "name"
     t.text "address"
     t.integer "time_budget"
@@ -32,14 +31,14 @@ ActiveRecord::Schema.define(version: 2014_12_26_231925) do
     t.integer "sales_tax_customer_class_id"
     t.text "vat_id"
     t.text "supplier_number"
-    t.string "email", limit: 255
+    t.string "email"
   end
 
   create_table "document_numbers", force: :cascade do |t|
-    t.string "code", limit: 255
-    t.string "format", limit: 255
+    t.string "code"
+    t.string "format"
     t.integer "sequence"
-    t.string "last_number", limit: 255
+    t.string "last_number"
     t.date "last_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -70,19 +69,19 @@ ActiveRecord::Schema.define(version: 2014_12_26_231925) do
     t.decimal "net"
     t.decimal "value"
     t.decimal "total"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "invoices", force: :cascade do |t|
-    t.string "document_number", limit: 255
+    t.string "document_number"
     t.boolean "published"
     t.integer "customer_id"
     t.integer "attachment_id"
     t.integer "project_id"
     t.date "date"
-    t.string "cust_reference", limit: 255
-    t.string "cust_order", limit: 255
+    t.string "cust_reference"
+    t.string "cust_order"
     t.text "prelude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -94,13 +93,13 @@ ActiveRecord::Schema.define(version: 2014_12_26_231925) do
     t.date "due_date"
     t.decimal "sum_net"
     t.decimal "sum_total"
-    t.string "token", limit: 255
+    t.string "token"
     t.text "tax_note"
     t.index ["document_number"], name: "index_invoices_on_document_number", unique: true
   end
 
   create_table "products", force: :cascade do |t|
-    t.string "title", limit: 255
+    t.string "title"
     t.text "description"
     t.decimal "rate"
     t.integer "sales_tax_product_class_id"
@@ -109,7 +108,7 @@ ActiveRecord::Schema.define(version: 2014_12_26_231925) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "matchcode", limit: 255
+    t.string "matchcode"
     t.text "description"
     t.integer "time_budget"
     t.integer "bill_to_customer_id"
@@ -118,15 +117,15 @@ ActiveRecord::Schema.define(version: 2014_12_26_231925) do
   end
 
   create_table "sales_tax_customer_classes", force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
     t.text "invoice_note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "sales_tax_product_classes", force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "indicator_code", limit: 255
+    t.string "name"
+    t.string "indicator_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

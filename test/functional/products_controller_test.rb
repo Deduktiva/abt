ea62780d8c9916159c5449c49a1db110c975346a
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2025-08-03 04:43:04 +0200

    Add smoke tests for controllers

---
+++ test/functional/products_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+end

9947e16d12c07a5f084813c45426b4f4519dd8af
Chris Hofstaedtler <zeha@users.noreply.github.com>
2025-08-04 23:19:35 +0200

    Split E-Mail templates (#60)
    - Extract common email styling and structure into document_mailer layout templates
    - Convert invoice-specific CSS classes to generic document classes (document-highlight, document-number, etc.)

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,6 +1,5 @@
 class InvoiceMailer < ApplicationMailer
   def customer_email
-    @issuer = IssuerCompany.get_the_issuer!
     @invoice = params[:invoice]
     attachments[@invoice.attachment.filename] = {
       mime_type: @invoice.attachment.content_type,
@@ -9,18 +8,20 @@ class InvoiceMailer < ApplicationMailer
 
     if @invoice.customer.invoice_email_auto_enabled
       subject = @invoice.customer.invoice_email_auto_subject_template.gsub('$CUST_ORDER$', @invoice.cust_order).gsub('$CUST_REF$', @invoice.cust_reference)
+      to = @invoice.customer.invoice_email_auto_to
+    elsif @invoice.customer.email
+      subject = "#{@issuer.short_name} Invoice #{@invoice.document_number}"
+      to = @invoice.customer.email
+    else
+      to = nil
+    end
+
+    unless to.nil?
       mail(
-        to: @invoice.customer.invoice_email_auto_to,
+        to: to,
         from: "\"#{@issuer.short_name}\" <#{@issuer.document_email_from}>",
         bcc: @issuer.document_email_auto_bcc,
         subject: subject
-      )
-    elsif @invoice.customer.email
-      mail(
-        to: @invoice.customer.email,
-        from: "\"#{@issuer.short_name}\" <#{@issuer.document_email_from}>",
-        bcc: @issuer.document_email_auto_bcc,
-        subject: "#{@issuer.short_name} Invoice #{@invoice.document_number}"
       )
     end
   end

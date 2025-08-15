a738c4a1651f5c4af6c356a86388597d6f47d509
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2025-08-15 08:32:08 +0200

    Setup SolidCache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,8 +53,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # Use Solid Cache (database-backed)
+  config.cache_store = :solid_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
